83f350c981c3ca4bfe54eb9e101c54202dea777e
Dima Kondratenko <kndrt_dima@ukr.net>
2021-01-10 16:35:05 +0200

    Исправил hw16

--- hw16/main-results.js
+++ hw16/main-results.js
@@ -170,4 +170,111 @@ function ageClassification(num) {
             :'молодой возраст'
           : 'детский возраст'
         : null
-    
+}
+
+console.log('-1 :', ageClassification(-1)); // -1 : null
+console.log('5 :', ageClassification(5)); // 5 : детский возраст
+console.log('34 :', ageClassification(34)); // 34 : молодой возраст
+console.log('50 :', ageClassification(50)); // 50 : средний возраст
+console.log('65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
+console.log('80 :', ageClassification(80)); // 80 : старческий возраст
+console.log('110 :', ageClassification(110)); // 110 : долгожители
+console.log('130 :', ageClassification(130)); // 130 : null
+
+/*
+ * Блок тестирования:
+ * console.log(1, ageClassification(-1) === null);
+ * console.log(2, ageClassification(1) === 'детский возраст');
+ * console.log(3, ageClassification(24) === 'детский возраст');
+ * console.log(4, ageClassification(24.01) === 'молодой возраст');
+ * console.log(5, ageClassification(44) === 'молодой возраст');
+ * console.log(6, ageClassification(44.01) === 'средний возраст');
+ * console.log(7, ageClassification(65) === 'средний возраст');
+ * console.log(8, ageClassification(65.01) === 'пожилой возраст');
+ * console.log(9, ageClassification(75) === 'пожилой возраст');
+ * console.log(10, ageClassification(75.01) === 'старческий возраст');
+ * console.log(11, ageClassification(90) === 'старческий возраст');
+ * console.log(12, ageClassification(90.01) === 'долгожители');
+ * console.log(13, ageClassification(122) === 'долгожители');
+ * console.log(14, ageClassification(122.01) === null);
+ * console.log(15, ageClassification(150) === null);
+ */
+
+/*
+ * #7
+ *
+ * Создайте функцию oddFn(n), которая принимает параметром число – количество итераций цикла.
+ * Функция должна вернуть массив, состоящий только из нечетных значений, генерируемых в цикле.
+ *
+ * Причем:
+ * 0 не должен попадать в результирующий массив
+ * цикл должен работать до n включительно
+ * разрешен только оператор while
+ *
+ * Например:
+ * oddFn(10) → [1, 3, 5, 7, 9]
+ * oddFn(15) → [1, 3, 5, 7, 9, 11, 13, 15]
+ * oddFn(20) → [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+ */
+
+function oddFn(n) {
+  let arr = [];
+  let i = 0;
+
+  while (i++ < n) if (i % 2 !== 0) arr.push(i);
+
+  return arr;
+}
+
+console.log(oddFn(10)); // [1, 3, 5, 7, 9]
+console.log(oddFn(15)); // [1, 3, 5, 7, 9, 11, 13, 15]
+console.log(oddFn(20)); // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+/*
+ * #8
+ *
+ * Создайте основную функцию mainFunc(a, b, func), которая принимает три параметра:
+ * a – число
+ * b - число
+ * func –  обрабатывающая параметры a и b, возвратная (callback) функция
+ *
+ * Реализуйте проверку: если третьим параметром передается не функция, нужно вернуть false.
+ *
+ */
+function mainFunc(a, b, cb) {
+  if (cb && typeof cb === 'function') return cb(a, b);
+
+  return false;
+}
+
+/*
+ * реализуйте следующие функции, которые будут осуществлять механизм callback в основной функции,
+ * возвращая ей результат собственного вычисления...
+ * для возведения в степень и получения произвольного значения можете воспользоваться методами объекта Math.
+ */
+
+// cbRandom(a, b) – вычисляет и возвращает произвольное целое число в диапазоне между a и b включительно.
+function cbRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// cbPow(a, b) – вычисляет и возвращает результат возведения числа a в степень b.
+function cbPow(num, pow) {
+  return Math.pow(num, pow);
+}
+// cbAdd(a, b) – вычисляет и возвращает сумму двух чисел a и b.
+function cbAdd(a, b) {
+  return a + b;
+}
+
+/*
+ * mainFunc() должна возвращать результат работы переданной ей возвратной функции, например:
+ * mainFunc(2, 5, cbRandom) → случайно от 2 до 5 включительно
+ * mainFunc(10, 30, cbRandom) → случайно 10..30 включительно
+ * mainFunc(2, 5, cbPow) → 32
+ * mainFunc(2, 5, cbAdd) → 7
+ * mainFunc(2, 5, 'not a func') → false
+ */
+console.log(mainFunc(2, 5, cbRandom)); // целые числа в диапазоне 2..5
+console.log(mainFunc(2, 5, cbPow)); // 32
+console.log(mainFunc(2, 5, cbAdd)); // 7
+console.log(mainFunc(2, 5, 'not a func')); // false
